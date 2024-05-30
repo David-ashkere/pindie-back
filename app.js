50fb33abe96aca16bdc6914117a128c92338a1f8
David-ashkere <149865934+David-ashkere@users.noreply.github.com>
2024-05-30 22:36:36 +0300

    sg

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("./middlewares/cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectToDatabase = require("./database/connect");
+
+const apiRouter = require("./routes/apiRouter");
+const pagesRouter = require("./routes/pages");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const cookieParser = require("cookie-parser");
+
+const PORT = 3001;
+
+connectToDatabase();
+
+app.use(
+  cors,
+  cookieParser(),
+  bodyParser.json(),
+  pagesRouter,
+  apiRouter,
+  express.static(path.join(__dirname, "/public"))
+);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
